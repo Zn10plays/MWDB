e5f02a2ea3a0b09a1530f136ec00fe71e574f3c9
Chase C. <rudrapatel26122004@gmail.com>
2024-08-13 21:19:13 -0400

    added support for minio

--- scripts/initdb.py
+++ scripts/initdb.py
@@ -1,7 +1,31 @@
 from connector import db
 from connector.orm import Base
+from minio import Minio
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
 
 def init():
+    init_sql_db()
+    init_minio()
+    pass
+
+def init_minio():
+    client = Minio('localhost:'+os.environ.get('MINIO_PORT'),
+                access_key=os.environ.get('MINIO_ROOT_USER'),
+                secret_key=os.environ.get('MINIO_ROOT_PASSWORD'),
+                secure=False)
+    try:
+        if not client.bucket_exists('novels'):
+            client.make_bucket('novels')
+    except Exception as e:
+        print(f'failed to create minio bucket: {e}')
+        pass
+    pass
+
+def init_sql_db():
     session = db.get_session()
 
     try:
@@ -12,6 +36,8 @@ def init():
         raise e
     finally:
         session.close()
+
+    pass
 
 if __name__ == '__main__':
     init()
