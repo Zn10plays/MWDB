8a325a7c4636b3b63323d91fd94041f54ea5cc02
chase <rudrapatel26122004@gmail.com>
2024-08-20 14:18:38 -0400

    parse error fix

--- connector/db.py
+++ connector/db.py
@@ -5,7 +5,7 @@ import os
 
 dotenv.load_dotenv()
 
-engine = create_engine(os.getenv('SQL_DATABASE_URL'), echo=os.environ.get('DEV'))
+engine = create_engine(os.getenv('SQL_DATABASE_URL'), echo=bool(os.getenv('DEV')))
 
 def get_session():
     return Session(bind=engine)
